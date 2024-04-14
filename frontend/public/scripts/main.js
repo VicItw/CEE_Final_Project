c7f1ae124439e784a16faa62a873735947e87b3c
MaximusGyraditus <kanisornputta@gmail.com>
2024-04-14 15:44:01 +0700

    popcat done

--- frontend/public/scripts/main.js
+++ frontend/public/scripts/main.js
@@ -1,23 +1,66 @@
-import { handleCreateMember, populateMembers } from "./member.js";
-import { fetchAndDrawTable, handleCreateItem, handleFilterItem } from "./table.js";
+// import { handleCreateMember, populateMembers } from "./member.js";
+// import { fetchAndDrawTable, handleCreateItem, handleFilterItem } from "./table.js";
 
-document.addEventListener("DOMContentLoaded", () => {
-  fetchAndDrawTable();
+// document.addEventListener("DOMContentLoaded", () => {
+//   fetchAndDrawTable();
 
-  populateMembers();
+//   populateMembers();
 
-  const addItemButton = document.getElementById("add-newrow");
-  addItemButton.addEventListener("click", () => {
-    handleCreateItem();
-  });
+//   const addItemButton = document.getElementById("add-newrow");
+//   addItemButton.addEventListener("click", () => {
+//     handleCreateItem();
+//   });
 
-  const filterButton = document.getElementById("filter-button");
-  filterButton.addEventListener("click", () => {
-    handleFilterItem();
-  });
+//   const filterButton = document.getElementById("filter-button");
+//   filterButton.addEventListener("click", () => {
+//     handleFilterItem();
+//   });
 
-  const addMemberButton = document.getElementById("add-member");
-  addMemberButton.addEventListener("click", () => {
-    handleCreateMember();
-  });
-});
+//   const addMemberButton = document.getElementById("add-member");
+//   addMemberButton.addEventListener("click", () => {
+//     handleCreateMember();
+//   });
+// });
+
+
+window.onload = function() {
+    var img = document.getElementById("pop-img");
+    var img_path = document.getElementById("pop-img").src; 
+    var pop = document.getElementById("pop-img").src; 
+    var count = document.getElementById("player-score");
+    var score = 0;
+    var clickCount = 0;
+    var currentCPS = 0;
+    var audio = new Audio("res/BOB.mp3");
+    var hit = 1;
+    var cpsCounter = document.getElementById("cps");
+
+    // click
+    img.addEventListener('mousedown', function () {
+        pop = img_path.replace("idle", "pop");
+        if ( currentCPS >= 10) {
+            pop = img_path.replace("idle", "ultra");
+        }
+        img.src = pop;
+        clickCount++;
+        increaseScore(hit);
+        console.log(pop);
+        audio.play()
+    }); 
+
+    // unclick
+    img.addEventListener('mouseup', function (){
+        img.src = img_path;
+    })
+
+    setInterval(function() {
+        cpsCounter.innerHTML = clickCount + " cps";
+        currentCPS = clickCount;
+        clickCount = 0; // Reset click count for the next interval
+      }, 1000); // Update CPS every 1000ms (1 second)
+
+    function increaseScore(hit) {
+        score += hit;
+        count.innerHTML = score;
+    }
+}
