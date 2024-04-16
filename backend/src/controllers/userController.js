d191023cef439be33878b2ab21084ad9de745876
Wutthichod <157282635+Wutthichod@users.noreply.github.com>
2024-04-16 13:54:28 +0700

    basic user route, model and controller

---
+++ backend/src/controllers/userController.js
@@ -0,0 +1,54 @@
+import User from "../models/userModel.js";
+
+export const getUser = async (req, res) => {
+  const user = await User.find({name: req.body.name});
+
+  res.status(200).json(user);
+};
+
+export const getRank = async (req, res) => {
+  const rankedUser = await User.find({ $sort: { score: 1 } }, { $limit: 10 });
+
+  res.status(200).json(rankedUser)
+
+}
+
+export const getAllUsers = async (req, res) => {
+  const users = await User.find();
+
+  res.status(200).json(users);
+};
+
+export const updateScore = async (req, res) => {
+
+  try {
+    const user = await User.update({name: req.body.name} , {$set: {score : req.body.score} } );
+
+    res.status(200).json({ message: "OK" });
+  } catch (err) {
+    if (err.name === "ValidationError") {
+      res.status(400).json({ error: "Bad Request" });
+    } else {
+      res.status(500).json({ error: "Internal server error." });
+    }
+  }
+};
+
+export const createUser = async (req, res) => {
+  try {
+    const newUser = new User(req.body);
+    if (User.find({name: newUser.name}).length === 0) {
+        await newUser.save();
+        res.status(200).json({ message: "OK" });
+    } else {
+        res.status(400).json({ message: "User name is already taken."})
+    }
+
+  } catch (err) {
+    if (err.name === "ValidationError") {
+      res.status(400).json({ error: "Bad Request" });
+    } else {
+      res.status(500).json({ error: "Internal server error." });
+    }
+  }
+};
