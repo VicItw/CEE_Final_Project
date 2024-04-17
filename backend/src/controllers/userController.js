11bcb4859b86bef2d5c31fe1f4d0359b9b8aefab
senella02 <thananan.pl@gmail.com>
2024-04-17 22:11:37 +0700

    api updated

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -1,50 +1,37 @@
 import User from "../models/userModel.js";
 
-export const getUser = async (req, res) => {
-  const user = await User.find({name: req.body.name});
-
+export const getUser = async (req, res) => { //ถ้าหาเจอ return json หาไม่เจอ return 400
+  const user = await User.find({name: req.params.user});
+  console.log(user.length);
+  if(user.length === 0){
+    res.status(400).json({error : "This Username is not available"});
+  }
   res.status(200).json(user);
 };
 
+
 export const getRank = async (req, res) => {
   const rankedUser = await User.find({ $sort: { score: 1 } }, { $limit: 10 });
-
   res.status(200).json(rankedUser)
-
 }
 
-export const getAllUsers = async (req, res) => {
-  const users = await User.find();
-
-  res.status(200).json(users);
-};
-
 export const updateScore = async (req, res) => {
-
   try {
-    const user = await User.update({name: req.body.name} , {$set: {score : req.body.score} } );
-
+    const user = await User.updateOne({name: req.body.name} , {$set: {score : req.body.score} } );
     res.status(200).json({ message: "OK" });
   } catch (err) {
-    if (err.name === "ValidationError") {
-      res.status(400).json({ error: "Bad Request" });
-    } else {
-      res.status(500).json({ error: "Internal server error." });
-    }
+    res.status(500).json({ error: "Internal server error." });
   }
 };
 
 export const createUser = async (req, res) => {
-  try {
-    const newUser = new User(req.body);
-    await newUser.save();
-    res.status(200).json({ message: "OK" });
-
-  } catch (err) {
-    if (err.name === "ValidationError") {
-      res.status(400).json({ error: "Bad Request" });
-    } else {
-      res.status(500).json({ error: "Internal server error." });
+    const user = await User.find({name : req.body.name});
+    if(user.length > 0){
+      res.status(401).json({error : "This Username is available"});
     }
-  }
+    else{
+      const newUser = new User(req.body);
+      await newUser.save();
+      res.status(200).json({ message: "OK" });
+    }
 };
